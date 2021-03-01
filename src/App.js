2e67c6ed2a4eb2983a8ed9bc4c6004f949ceca17
jganchozo <jganchozo@outlook.com>
2021-03-01 12:24:10 -0500

    mensaje condicional

--- src/App.js
+++ src/App.js
@@ -18,6 +18,9 @@ function App() {
     guardarCitas(nuevasCitas);
   }
 
+  //mensaje condicional
+  const titulo = citas.length === 0 ? 'No hay citas': 'Administra tus Citas';
+
   return (
     <Fragment>
       <h1>Administrador de Pacientes</h1>
@@ -28,7 +31,7 @@ function App() {
             <Formulario crearCita={crearCita}/>
           </div>
           <div className="one-half column">
-            <h2>Administra tus citas</h2>
+            <h2>{titulo}</h2>
             {citas.map(cita => (
               <Cita
                 key={cita.id} 
