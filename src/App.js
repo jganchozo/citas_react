844a9a2d817c6842e21075e41a8d1223f7ff0ad0
jganchozo <jganchozo@outlook.com>
2021-03-01 12:03:19 -0500

    mostrar citas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Fragment, useState } from "react";
 import Formulario from './components/Formulario';
+import Cita from './components/Cita';
 
 function App() {
 
@@ -22,7 +23,13 @@ function App() {
             <Formulario crearCita={crearCita}/>
           </div>
           <div className="one-half column">
-            2
+            <h2>Administra tus citas</h2>
+            {citas.map(cita => (
+              <Cita
+                key={cita.id} 
+                cita={cita}
+              />
+            ))}
           </div>
         </div>
       </div>
